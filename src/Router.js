0d5057576db733967f6ef662c320050fe1f5c362
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-04-23 11:21:36 -0700

    added hash routing

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import {
-  Router
+  Router,
+  createHistory,
+  LocationProvider
 } from "@reach/router"
+import uuid from 'uuid/v4'
+import createHashSource from 'hash-source'
 
 import Header from './Header'
 import Talks from './Talks'
 import TalkComments from './TalkComments'
-
 import { TalkModalContext, ClientIDContext } from './contexts'
-import uuid from 'uuid/v4'
 
+const source = createHashSource()
+const history = createHistory(source)
 const CLIENT_ID = uuid()
 
 const AppRouter = ({ children }) => (
@@ -26,22 +30,24 @@ function App() {
   }
 
   return (
-    <TalkModalContext.Provider value={{
-      modalVisible,
-      toggle
-    }}>
-      <ClientIDContext.Provider value={{ CLIENT_ID }}>
-        <div>
-          <Header />
-          <Router>
-            <AppRouter path="/">
-              <Talks path="/" />
-              <TalkComments path="/talk/:talkId/:talkName" />
-            </AppRouter>
-          </Router>
-        </div>
-      </ClientIDContext.Provider>
-    </TalkModalContext.Provider>
+    <LocationProvider history={history}>
+      <TalkModalContext.Provider value={{
+        modalVisible,
+        toggle
+      }}>
+        <ClientIDContext.Provider value={{ CLIENT_ID }}>
+          <div>
+            <Header />
+            <Router>
+              <AppRouter path="/">
+                <Talks path="/" />
+                <TalkComments path="/talk/:talkId/:talkName" />
+              </AppRouter>
+            </Router>
+          </div>
+        </ClientIDContext.Provider>
+      </TalkModalContext.Provider>
+    </LocationProvider>
   )
 }
 
